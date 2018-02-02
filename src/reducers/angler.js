3f15bacabd9cf5be1d936c1c511ce42fad14b186
rahul4times <tkdmaster.sharma@gmail.com>
2018-02-02 12:48:39 -0700

    reducers done

---
+++ src/reducers/angler.js
@@ -0,0 +1,15 @@
+import {
+  GET_ANGLER_PENDING,
+  GET_ANGLER_SUCCESS
+} from '../actions/angler';
+
+export default (state=[], action) => {
+  switch(action.type){
+    case GET_ANGLER_PENDING:
+      return state;
+    case GET_ANGLER_SUCCESS:
+      return [...action.payload.data]
+    default:
+        return state;
+  }
+}
