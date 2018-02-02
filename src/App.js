04ec1731d4b542cdd5db75fc9ff28b7d230eaf20
rahul4times <tkdmaster.sharma@gmail.com>
2018-02-02 12:35:03 -0700

    packages added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        code-kata
+      </div>
+    );
+  }
+}
+
+export default App;
