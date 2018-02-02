386d972eb08ba6431d48d0e1891996be6053b6c6
rahul4times <tkdmaster.sharma@gmail.com>
2018-02-02 12:59:50 -0700

    store is done, app component wrapped in provider

--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Store from './store'
+import { Provider } from 'react-redux';
+
+const storeInstance = Store();
+
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
